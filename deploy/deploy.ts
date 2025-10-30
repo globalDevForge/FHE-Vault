4e528b903dd39b0f371fc2693d763f30c3169b0a
globalDevForge <zvcrxr709@outlook.com>
2025-10-30 13:38:11 +0800

    deploy

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,20 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const token = await deploy("ERC7984USDT", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  const vault = await deploy("FHEVault", {
+    from: deployer,
+    args: [token.address],
+    log: true,
+  });
+
+  console.log(`ERC7984USDT contract: ${token.address}`);
+  console.log(`FHEVault contract: ${vault.address}`);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_fheVault"; // id required to prevent reexecution
+func.tags = ["FHEVault"];
